3538920c91fcfbaf30aa6d72305c3496e22c2450
Bigblockkak <fillar.vincent02@gmail.com>
2025-09-23 13:58:10 +0800

    new

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = 4000;
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://myportfolio-roan-zeta.vercel.app"
+  "https://vincentfillar.vercel.app/"
 ];
 
 app.use(cors({
